f5f069743566a735f5fdc66f7bb38814f4b0f044
vanshgoyalatgithub <vansh.goel.0403@gmail.com>
2024-07-15 14:14:08 +0530

    added useeffct hooks and usestate hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
+import api from './api/axiosConfig';
+import { useEffect,useState } from 'react';
 function App() {
+
+  const [movie, setMovie] = useState([]);
+  const getMovie =async () => {
+    
+    
+  try {
+    const response = await api.get('/api/');
+    console.log(response.data);
+    setMovie(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+  }
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    getMovie();
+
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
---
+++ src/api/axiosConfig.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+// export default axios.create({
+//     baseURL: 'http://localhost:3000',
+//     headers: {
+//         'Content-type': 'application/json',
+//     },
+// });
+export default axios.create({
+    baseURL:'http://localhost:8080/',
+    headers: {"ngrok-skip-browser-warning": "true"}
+});
