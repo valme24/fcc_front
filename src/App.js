3bd716d80b6658c4ef47bdf238fcac501d9d7772
vanshgoyalatgithub <vansh.goel.0403@gmail.com>
2024-07-17 15:16:59 +0530

    updting project for sharing

--- src/App.js
+++ src/App.js
@@ -26,9 +26,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home movies={movies} />} />
-        </Route>
+      <Route path="*" element={<Layout/>}>
+            <Route path="movie" element={<Home movies={movies} />} ></Route>
+            {/* <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route> */}
+            {/* <Route path="*" element = {<NotFound/>}></Route> */}
+          </Route>
       </Routes>
     </div>
   );
